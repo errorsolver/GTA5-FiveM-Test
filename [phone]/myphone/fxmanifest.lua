c530d00be71b12ed900ab0e22cec9cca767b89ff
Vincentius Hadianta Wibawa <72688274+errorsolver@users.noreply.github.com>
2025-03-19 15:48:30 +0700

    teleport

---
+++ [phone]/myphone/fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+game 'gta5'
+
+ui_page 'html/index.html'
+
+files {
+    'html/index.html',
+    'html/css/*.css',
+    'html/js/*.js'
+}
+
+client_script 'client.lua'
+server_script 'server.lua'
--- [tutorial]/ch_nui/client/client.lua
+++ [tutorial]/ch_nui/client/client.lua
@@ -13,11 +13,18 @@ RegisterNUICallback('releaseFocus', function(data, cb)
 end)
 
 RegisterNUICallback('teleport', function(data, cb)
-    cb({})
+    local x_pos = assert(tonumber(data.x), infoToPlayer('X invalid'))
+    local y_pos = assert(tonumber(data.y), infoToPlayer('Y invalid'))
+    local z_pos = assert(tonumber(data.z), infoToPlayer('Z invalid'))
 
-    local x_pos = assert(tonumber(data.x), 'x is required')
-    local y_pos = assert(tonumber(data.x), 'y is required')
-    local z_pos = assert(tonumber(data.x), 'z is required')
-    
     SetEntityCoords(PlayerPedId(), x_pos, y_pos, z_pos, false, false, false, false)
+    cb({}) -- Safety, callback must be in bottom
 end)
+
+function infoToPlayer(args)
+    TriggerEvent('chat:addMessage', {
+        color = {255, 0, 0},
+        multiline = true,
+        args = {args}
+    })
+end
